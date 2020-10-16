c33d498899107804567a5e556d9ffb47330fe8c6
御剑 <61449590+ttt2021@users.noreply.github.com>
2020-10-16 23:22:02 +0800

    函数调用模式

--- readbook/jsEssenceLanguage/code/1.js
+++ readbook/jsEssenceLanguage/code/1.js
@@ -1,5 +1,4 @@
 //  函数的方法调用模式
-
 var myObj = {
     value: 0,
     increment: function (inc) {
@@ -8,7 +7,31 @@ var myObj = {
 }
 myObj.increment()
 // document.writeln(myObj.value)  // 1
-console.log(myObj.value)
+// console.log(myObj.value)
 myObj.increment(2)
 // document.writeln(myObj.value)  // 3
-console.log(myObj.value)
+// console.log(myObj.value)
+
+var add = function (a, b) {
+    return a + b
+}
+
+// 函数调用模式
+var sum = add(3, 4) // 7
+myObj.double = function () {
+    var that = this 
+    // console.log(that)  // 1 3
+    
+    // console.log(that.value)
+    var helper = function (){
+        // console.log(that.value)  // 3
+        that.value = add(that.value, that.value)
+    }
+
+    helper() // 以函数的形式调用 helper
+    // console.log(helper()) // undefined
+}
+
+// 以方法的形式调用 double
+myObj.double() // undefined
+console.log(myObj.value) // 6
