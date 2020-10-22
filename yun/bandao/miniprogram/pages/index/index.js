45e13fb35a8b88354853cd4a49df7eb5420e85c2
ttt2021 <2815313941@qq.com>
2020-10-22 21:29:14 +0800

    bandao

--- yun/bandao/miniprogram/pages/index/index.js
+++ yun/bandao/miniprogram/pages/index/index.js
@@ -3,17 +3,63 @@ const db = wx.cloud.database() // wx 微信服务的命名空间 database 数据
 const _ = db.command // CRUD
 // table collection
 const productsCollection = db.collection('products')
+// 图片表
+const photos = db.collection('photos')
 
+// wx 微信 database 数据库
 const app = getApp()
 
 Page({
   data: {
     products: [],
+    photos: [],
     avatarUrl: './user-unlogin.png',
     userInfo: {},
     logged: false,
     takeSession: false,
     requestResult: ''
+  },
+
+  // 上传图片
+  upload() {
+    // console.log('点了按钮')
+    // 云开发，SQL
+    // html
+    // weixin 给予小程序能力
+    // 在相机里选择
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        // console.log(res)
+        const tempFilePaths = res.tempFilePaths
+        for (var i = 0; i < tempFilePaths.length; i++) {
+          let randString = + new Date() + '' + Math.floor(Math.random * 1000000) + '.png'  // +new Date() 时间戳转换
+          wx.cloud.uploadFile({
+            cloudPath: randString,
+            filePath: tempFilePaths[i],
+            success: res => {
+              if (res.statusCode == 200) {
+                // 增加数据
+                photos.add({
+                  data: {
+                    image: res.fileID
+                  }
+                })
+                .then(res => {
+                  wx.showToast({
+                    title: '上传成功',
+                    icon: 'success'
+                  })
+                }) 
+              }
+              // console.log(res)
+            }
+          })
+        }
+      }
+    })
   },
 
   onLoad: function() {
@@ -22,6 +68,13 @@ Page({
         this.setData({
           products: res.data
         })
+    })
+    // 获取图片
+    photos.get().then(res => {
+      console.log(res)
+      this.setData({
+        photos: res.data
+      })
     })
   },
 
