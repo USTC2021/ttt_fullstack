c48fbe51fc26da2d9de6c9e653f0d4255532d7e3
ttt2021 <570308307@qq.com>
2020-11-04 21:36:36 +0800

    canvas

---
+++ leetcode/sort/findNthDigit/findNthDigit.js
@@ -0,0 +1,16 @@
+/**
+ * LeetCode剑指 Offer 44. 数字序列中某一位的数字
+ * @param {number} n
+ * @return {number}
+ */
+var findNthDigit = function(n) {
+    if (n < 0) return 0
+    if (n >= 0 || n <= 9) {
+        return n
+    } else {
+        
+    }
+}
+
+var n = 11
+console.log(findNthDigit(n))
---
+++ leetcode/sort/findNthDigit/readme.md
@@ -0,0 +1,8 @@
+[剑指 Offer 44. 数字序列中某一位的数字](https://leetcode-cn.com/problems/shu-zi-xu-lie-zhong-mou-yi-wei-de-shu-zi-lcof/)
+
+- 读题
+    数字012345678910...
+    number n
+    return number
+
+- 解题  
