1b3b307cff97757a5862691117b732afc3ebcbf1
ttt2021 <570308307@qq.com>
2020-11-06 17:25:44 +0800

    barrage

---
+++ leetcode/sort/findContinuousSequence/findContinuousSequence.js
@@ -0,0 +1,14 @@
+/**
+ * LeetCode剑指 Offer 57 - II. 和为s的连续正数序列
+ * @param {number} target
+ * @return {number[][]}
+ */
+var findContinuousSequence = function(target) {
+    
+}
+
+var target = 9
+console.log(findContinuousSequence(target))
+
+// var target = 15
+// console.log(findContinuousSequence(target))
