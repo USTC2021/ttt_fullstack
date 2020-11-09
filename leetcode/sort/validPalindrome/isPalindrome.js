95c4557042d965c875d4a61c9e7fde86a0326c67
ttt2021 <570308307@qq.com>
2020-11-09 19:24:47 +0800

    leetcode

--- leetcode/sort/validPalindrome/isPalindrome.js
+++ leetcode/sort/validPalindrome/isPalindrome.js
@@ -5,25 +5,25 @@
  */
 var isPalindrome = function(s) {
     // 将字符串中非数字和字母进行替换
-    // s = s.replace(/[\W|_]/g, '')
-    // // s = s.replace(/[^0-9a-zA-Z]/g, '')
-    // // console.log(s)
-    // // 将字符串转成小写
-    // s = s.toLowerCase()
-    // // console.log(s)
-    // // 将字符串转成数组
-    // result = s.split('')
-    // // console.log(result)
-    // // 将数组翻转
-    // result = result.reverse()
-    // // console.log(result)
-    // // 将数组转换成字符串
-    // result = result.join('')
-    // // console.log(result)
-    // if (s === result) return true
-    // return false
-    s = s.replace(/[^0-9a-zA-Z]/g, '').toLowerCase()
-    return s === s.split('').reverse().join('')
+    s = s.replace(/[\W|_]/g, '')
+    // s = s.replace(/[^0-9a-zA-Z]/g, '')
+    // console.log(s)
+    // 将字符串转成小写
+    s = s.toLowerCase()
+    // console.log(s)
+    // 将字符串转成数组
+    result = s.split('')
+    // console.log(result)
+    // 将数组翻转
+    result = result.reverse()
+    // console.log(result)
+    // 将数组转换成字符串
+    result = result.join('')
+    // console.log(result)
+    if (s === result) return true
+    return false
+    // s = s.replace(/[^0-9a-zA-Z]/g, '').toLowerCase()
+    // return s === s.split('').reverse().join('')
 }
 
 var arr = 'A man, a plan, a canal: Panama'
