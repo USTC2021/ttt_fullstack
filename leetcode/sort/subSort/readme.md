531c010cd3f3a9a18e5499b7700df7cb3078a0d5
ttt2021 <570308307@qq.com>
2020-11-09 19:12:21 +0800

    leetcode

---
+++ leetcode/sort/subSort/readme.md
@@ -0,0 +1,6 @@
+[面试题 16.16. 部分排序](https://leetcode-cn.com/problems/sub-sort-lcci/)
+
+- 读题
+    
+
+- 解题
---
+++ leetcode/sort/subSort/subSort.js
@@ -0,0 +1,12 @@
+/**
+ * LeetCode面试题 16.16. 部分排序
+ * @param {number[]} array
+ * @return {number[]}
+ */
+
+var subSort = function(array) {
+
+}
+
+var arr = [1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]
+console.log(subSort(arr))
