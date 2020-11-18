4c862a670e210410ec1b599cba72b1210201bef6
ttt2021 <570308307@qq.com>
2020-11-18 17:41:50 +0800

    leetcode

--- yun/book/miniprogram/pages/bookSection/bookSection.js
+++ yun/book/miniprogram/pages/bookSection/bookSection.js
@@ -1,11 +1,21 @@
 // miniprogram/pages/bookSection/bookSection.js
+const db = wx.cloud.database()
+const app = getApp()
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-        bookDetailData: {}
+        bookDetailData: {},
+        lastData: [],
+        pageData: [],
+        pageArray: [],
+        page: 1,
+        pre: '',
+        next: '',
+        preAble: false,
+        nextAble: false
     },
 
     getSection(url) {
@@ -22,8 +32,104 @@ Page({
             wx.hideLoading()
             const { result } = res
             this.setData({
-                bookDetailData: result.bookDetailData
+                bookDetailData: result.bookDetailData,
+                lastData: result.lastData,
+                pageData: result.pageData,
+                pageArray: result.pageArray,
+                pre: result.pre,
+                next: result.next,
+                preAble: result.pre === '' ? true : false,
+                nextAble: result.next === '' ? true : false,
+                page: (result.next.split('/')[2]) - 1
             })
+        })
+    },
+
+    // 上一页
+    prePage() {
+        if (this.data.preAble) return
+        this.getSection(this.data.pre)
+    },
+    
+    nextPage() {
+        this.getSection(this.data.next)
+    },
+
+    bindPickerChange(e) {
+        console.log(e.detail.value)
+        let index = parseInt(e.detail.value)
+        let url = this.data.pageArray[index].name
+        console.log(url.split('/')[2])
+        if (index !== this.page) {
+            this.getSection(url)
+            this.setData({
+                page: index + 1
+            })
+        }
+    },
+
+    // 去看小说
+    navtoUrl(e) {
+        console.log(e)
+        let url = e.currentTarget.dataset.url
+        console.log(url)
+        
+        // 已经存在书架的书，记录阅读状态
+        if (url) {
+            db.collection('book').where({
+                userId: app.globalData._openid,
+                bookName: this.data.bookDetailData.name
+            }).get().then(res => {
+                let data = res.data || []
+                if (data.length > 0) {
+                    if (data[0].bookUrl !== url) {
+                        const id = data[0]._id || ''
+                        db.collection('book').doc(id).update({
+                            data: {
+                                bookUrl: url
+                            }
+                        }).then(res => {
+                            console.log(res)
+                        })
+                    }
+                }
+            })
+        }
+
+        wx.navigateTo({
+          url: `../bookContent/bookContent?url=${url}&name=${this.data.bookDetailData.name}&imgUrl=${this.data.bookDetailData.imgurl}`,
+        })
+    },
+
+    joinBook(e) {
+        let url = e.currentTarget.dataset.url
+        db.collection('book').where({
+            userId: app.globalData._openid,
+            bookName: this.data.bookDetailData.name
+        }).get().then(res => {
+            console.log(res)
+            const data = res.data[0] || []
+            if (data.length == 0) { // 没有加入过书架
+                db.collection('book').add({ // 数据库添加数据
+                    data: {
+                        userId: app.globalData._openid,
+                        bookName: this.data.bookDetailData.name,
+                        bookUrl: url,
+                        imgurl: this.data.bookDetailData.imgurl
+                    }
+                }).then(res => {
+                    console.log(res)
+                    wx.showToast({
+                      icon: 'success',
+                      title: '加入成功',
+                    }, 3000)
+                })
+            } else {
+                wx.showToast({
+                  icon: 'none',
+                  title: '本书已在书架，请到书架阅读',
+                })
+            }
         })
     },
 
