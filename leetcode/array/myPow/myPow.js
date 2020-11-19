65fdb5358e2bf939c737a4948e95ecd1cc148dd7
ttt2021 <570308307@qq.com>
2020-11-19 18:42:36 +0800

    leetcode

---
+++ leetcode/array/myPow/myPow.js
@@ -0,0 +1,37 @@
+/**
+ * leetcode剑指 Offer 16. 数值的整数次方
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+
+ // 超时
+// var myPow = function(x, n) {
+//     var num = 1
+//     for (var i = 0; i < Math.abs(n); i++) 
+//             num *= x
+//     if (n < 0) {
+//         num = 1 / num
+//     }
+//     return num.toFixed(5)
+// }
+
+var myPow = (x, n) => x ** n // Math.pow(x, n)
+
+var x = 2.00000, n = 10
+console.log(myPow(x, n)) // 1024.00000
+
+var x = 2.00000, n = 1
+console.log(myPow(x, n)) // 2.00000
+
+var x = 2.00000, n = 0
+console.log(myPow(x, n)) // 1.00000
+
+var x = 2.10000, n = 3
+console.log(myPow(x, n)) // 9.26100
+
+var x = 2.00000, n = -2
+console.log(myPow(x, n)) // 0.25000
+
+var x = 2.00000, n = -2147483648
+console.log(myPow(x, n)) 
---
+++ leetcode/array/myPow/readme.md
@@ -0,0 +1,13 @@
+[剑指 Offer 16. 数值的整数次方](https://leetcode-cn.com/problems/shu-zhi-de-zheng-shu-ci-fang-lcof/)
+
+- 读题
+    number x  number n
+    实现x的n次方
+    不使用库函数
+    return number
+
+- 解题
+    1. 方法一
+    return x**n
+
+    2. 方法二：位运算
