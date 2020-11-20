55d13bbb99e8c9e95d61fac0051a180ca39878ac
ttt2021 <570308307@qq.com>
2020-11-20 10:02:16 +0800

    leetcode

--- leetcode/array/twoSum/readme.md
+++ leetcode/array/twoSum/readme.md
@@ -1,6 +1,17 @@
-[面试题60. n个骰子的点数](https://leetcode-cn.com/problems/nge-tou-zi-de-dian-shu-lcof/)
+[剑指 Offer 57. 和为s的两个数字](https://leetcode-cn.com/problems/he-wei-sde-liang-ge-shu-zi-lcof/)
 
 - 读题
-    
+    number[] nums   number target
+    递增数组 和s
+    查找两数正好和为s  输出一对和为s
+    return number[]
 
 - 解题
+    1. 双指针
+    - 定义两个指针，一个指向首元素，一个指向末尾元素
+    - 计算两指针指向元素的和
+    - 若其和大于目标值，则右指针往左移一位
+    - 若其和小于目标值，则左指针往右移一位
+    - 若相等，则以数组的形式返回所指向的元素
+
+    2. 暴力法 (超时)
--- leetcode/array/twoSum/twoSum.js
+++ leetcode/array/twoSum/twoSum.js
@@ -1,35 +1,67 @@
 /**
- * leetcode面试题60. n个骰子的点数
+ * leetcode剑指 Offer 57. 和为s的两个数字
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 
-// 数据过大，超时
 var twoSum = function(nums, target) {
-    var result = []
-    if (nums.length == 0) 
-        return result
-    for (var i = 0; i < nums.length; i++) {
-        var index = nums.indexOf(target - nums[i])
-        // console.log(num[i], index, num)
-        if (index != -1 && index > i){
-            result[0] = nums[i]
-            result[1] = nums[index]
-            break
-        }
+    // 数据过大，超时
+    // var result = []
+    // if (nums.length == 0) 
+    //     return result
+    // for (var i = 0; i < nums.length; i++) {
+    //     var index = nums.indexOf(target - nums[i])
+    //     // console.log(num[i], index, num)
+    //     if (index != -1 && index > i){
+    //         result[0] = nums[i]
+    //         result[1] = nums[index]
+    //         break
+    //     }
+    // }
+    // return result
+
+    // 数据过大，超时
+    // var result = []
+    // if (nums.length == 0) 
+    //     return result
+    // for (var i = 0; i < nums.length; i++) {
+    //     var rest = target - nums[i]
+    //     if (rest > nums[i]) {
+    //         for (var j = i + 1; j < nums.length; j++) {
+    //             if (rest == nums[j]) {
+    //                 result[0] = nums[i]
+    //                 result[1] = rest
+    //                 break
+    //             }
+    //         }
+    //     }
+    // }
+    // return result
+
+
+    // 双指针
+    let i = 0, j = nums.length - 1
+    while (i < j) {
+        let s = nums[i] + nums[j]
+        if (s > target) 
+            j--
+        if (s < target)
+            i++
+        if (s === target)
+            return [nums[i], nums[j]]
     }
-    return result
+    return []
 }
 
 var nums = [2, 7, 11, 15], target = 9
-console.log(twoSum(nums, target))
-
-var nums = [2, 9, 11, 15], target = 9
-console.log(twoSum(nums))
+console.log(twoSum(nums, target)) // [2, 7]
 
 var nums = [], target = 9
-console.log(twoSum(nums))
+console.log(twoSum(nums)) // []
+
+var nums = [2, 9, 11, 15], target = 9
+console.log(twoSum(nums)) // []
 
 var nums = [10, 26, 30, 31, 47, 60], target = 40
-console.log(twoSum(nums, target))
+console.log(twoSum(nums, target)) // [10, 30]
