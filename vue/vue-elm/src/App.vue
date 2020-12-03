837149f9ec6a9f22b0eac092f1f218212bd02322
ttt2021 <570308307@qq.com>
2020-12-03 20:24:49 +0800

    vue-elm

--- vue/vue-elm/src/App.vue
+++ vue/vue-elm/src/App.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="app">
-    <Header></Header>
+    <Header :seller="seller"></Header>
+    <div class="tab">
+      <div class="tab-wrapper">
+        <router-link to="/">商品</router-link>
+      </div>
+      <div class="tab-wrapper">
+        <router-link to="/comment">评论</router-link>
+      </div>
+      <div class="tab-wrapper">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    
+    <div class="page">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
 import Header from '@/components/header/Header'
+import { getSeller } from '@/api'
+import qs from 'query-string'
 export default {
+  data () {
+    return {
+      seller: {
+        id: qs.parse(location.search).id
+      }
+    }
+  },
   components: {
     Header
+  },
+  created () {
+    getSeller({
+      id: this.seller.id
+    }).then((seller) => {
+      console.log(seller)
+      this.seller = Object.assign({}, this.seller, seller)
+    })
   }
 }
 </script>
 
 <style lang="stylus">
-
+@import './common/stylus/variable.styl';
+.tab 
+  widows: 100%;
+  display flex
+  height: 36px;
+  line-height: 36px;
+  .tab-wrapper
+    flex 1
+    text-align: center;
+    a
+      width: 100%;
+      display: inline-block;
+      color: #666
+      text-decoration none
+    .router-link-exact-active
+      color $color-red
+      border-bottom: 2px solid $color-red;
 </style>
--- vue/vue-elm/src/router/index.js
+++ vue/vue-elm/src/router/index.js
@@ -1,10 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import goods from '@/views/goods/goods'
 
 Vue.use(VueRouter)
 
 const routes = [
-  
+  {
+    path: '/',
+    name: "goods",
+    component: goods
+  },
+  {
+    path: '/comment',
+    name: "comment",
+    component: () => import('@/views/comment/comment.vue') // 路由按需加载
+  },
+  {
+    path: '/seller',
+    name: "seller",
+    component: () => import('@/views/seller/seller.vue')
+  }
 ]
 
 const router = new VueRouter({
