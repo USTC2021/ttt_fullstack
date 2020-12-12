0e382c7994482ca6dd18f218ea54ab981fb577b2
ttt2021 <570308307@qq.com>
2020-12-12 19:27:07 +0800

    leetcode

--- leetcode/string/replaceSpace/replaceSpace.js
+++ leetcode/string/replaceSpace/replaceSpace.js
@@ -4,18 +4,32 @@
  * @return {string}
  */
 var replaceSpace = function(s) {
-  // if (s.length === 0)
-  //   return ''
+  // // if (s.length === 0)
+  // //   return ''
+  // for (let i = 0; i < s.length; i++) {
+  //   if (s[i] == ' ') 
+  //     s = s.replace(s[i], '%20')
+  // }
+  // // console.log(s)
+  // return s
+
+  // return s.replace(/\s/g, '%20')
+
+  if (s.length === 0)
+    return ''
+  let arr = []
   for (let i = 0; i < s.length; i++) {
-    if (s[i] == ' ') 
-      s = s.replace(s[i], '%20')
+    if (s[i] === ' ') {
+      arr.push('%20')
+    } else {
+      arr.push(s[i])
+    }
   }
-  // console.log(s)
-  return s
+  return arr.join('')
 }
 
-// let s = 'We are happy.'
-// console.log(replaceSpace(s)) // "We%20are%20happy."
+let s = 'We are happy.'
+console.log(replaceSpace(s)) // "We%20are%20happy."
 
-let s = ''
-console.log(replaceSpace(s)) // ''
+// let s = ''
+// console.log(replaceSpace(s)) // ''
