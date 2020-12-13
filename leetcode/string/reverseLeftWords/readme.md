47bf74b8e997d8879c7417692269a77f745ca5a6
ttt2021 <570308307@qq.com>
2020-12-13 15:31:38 +0800

    leetcode

---
+++ leetcode/string/reverseLeftWords/readme.md
@@ -0,0 +1,10 @@
+[剑指 Offer 58 - II. 左旋转字符串](https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof/)
+
+- 读题
+  string s
+  number n
+  将字符串前面的若干个字符转移到字符串的尾部
+  return string
+   
+- 解题
+  1. 方法一
---
+++ leetcode/string/reverseLeftWords/reverseLeftWords.js
@@ -0,0 +1,12 @@
+/**
+ * leetcode剑指 Offer 58 - II. 左旋转字符串
+ * @param {string} s
+ * @param {number} n
+ * @return {string}
+ */
+var reverseLeftWords = function(s, n) {
+
+}
+
+let s = 'lrloseumgh', k = 6
+console.log(reverseLeftWords(s, k)) // "umghlrlose"
