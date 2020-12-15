91fb99b8eeedf94a3a8933786eeb5f3ec0922888
ttt2021 <570308307@qq.com>
2020-12-15 21:17:05 +0800

    noteBook

---
+++ vue/notebook/notebook-client/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import 'amfe-flexible'
+import { Swipe, SwipeItem, Toast } from 'vant';
+import 'vant/lib/index.css';
+import axios from 'axios'
+import util from './assets/js/util'
+
+Vue.use(Swipe).use(SwipeItem).use(Toast).use(axios);
+
+Vue.config.productionTip = false
+Vue.prototype.$http = axios
+Vue.prototype.$util = util
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
