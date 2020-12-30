5d1dca61239e0e384eaaf6cc63a256191807e538
ttt2021 <570308307@qq.com>
2020-12-30 22:10:10 +0800

    vuex

---
+++ vue/my-vuex/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <!-- <p>{{this.$store.vm.state.num}}</p> -->
+    <p>{{this.$store.state.num}}</p>
+    <p>{{this.$store.getters.getNum}}</p>
+    <button @click="add">+1</button>
+    <button @click="asyncAdd">+2</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    add() {
+      this.$store.commit('add', 1)
+    },
+    asyncAdd() {
+      this.$store.dispatch('actionAdd', 2)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
