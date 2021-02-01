f106c1f2e8f3db68f979f5f7d8186a06bae4a780
ttt2021 <570308307@qq.com>
2021-02-01 21:25:37 +0800

    深度优先

---
+++ datastruct/数据结构/遍历/dfs.js
@@ -0,0 +1,29 @@
+function preorder (root) {
+  if (!root) return
+  console.log('当前遍历的结点值是:', root.val)
+  // 递归遍历左子树
+  preorder(root.left)
+  // 递归遍历右子树
+  preorder(root.right)
+}
+
+let root = {
+  val: 'A',
+  left: {
+    val: 'B',
+    left: {
+      val: 'D'
+    },
+    right: {
+      val: 'E'
+    }
+  },
+  right: {
+    val: 'C',
+    right: {
+      val: 'F'
+    }
+  }
+}
+
+preorder(root)
